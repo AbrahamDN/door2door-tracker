f5094acb6f41a72816e7c5fa70c089dd8657a759
AbrahamDN <abe.ndinga@gmail.com>
2025-02-03 05:12:32 +0000

    fix build issue for unused vars

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,19 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Configure @typescript-eslint/no-unused-vars
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          argsIgnorePattern: "^_", // Ignore parameters prefixed with `_`
+          varsIgnorePattern: "^_", // Ignore variables prefixed with `_`
+          caughtErrorsIgnorePattern: "^_", // Ignore caught errors prefixed with `_`
+        },
+      ],
+    },
+  },
 ];
 
 export default eslintConfig;
